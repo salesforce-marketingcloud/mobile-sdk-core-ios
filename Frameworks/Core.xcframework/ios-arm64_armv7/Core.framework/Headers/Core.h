8a8681804a3937eb4dfe99517dd7f6ac0e20f256
tok-sfci220 <tok_sfci220@salesforce.com>
2021-01-19 11:17:19 -0800

    1.0.0

---
+++ Frameworks/Core.xcframework/ios-arm64_armv7/Core.framework/Headers/Core.h
@@ -0,0 +1,22 @@
+//
+//  core.h
+//  core
+//
+//  Created by Tyler Roach on 6/29/20.
+//  Copyright © 2020 Salesforce Marketing Cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SFKeyStoreManager.h"
+
+
+//! Project version number for core.
+FOUNDATION_EXPORT double coreVersionNumber;
+
+//! Project version string for core.
+FOUNDATION_EXPORT const unsigned char coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <core/PublicHeader.h>
+
+
+
